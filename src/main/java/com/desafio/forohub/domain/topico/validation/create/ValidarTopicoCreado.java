de286a030861182689c9cc2250d98fd053de5f1d
FernandoDiaz01 <fdiaz4823@gmail.com>
2025-01-15 18:02:31 -0300

    Challenge Foro Hub finalizado

---
+++ src/main/java/com/desafio/forohub/domain/topico/validation/create/ValidarTopicoCreado.java
@@ -0,0 +1,7 @@
+package com.desafio.forohub.domain.topico.validation.create;
+
+import com.desafio.forohub.domain.topico.dto.CrearTopicoDTO;
+
+public interface ValidarTopicoCreado {
+    void validate(CrearTopicoDTO data);
+}
---
+++ src/main/java/com/desafio/forohub/domain/usuario/validation/create/ValidarCrearUsuario.java
@@ -0,0 +1,7 @@
+package com.desafio.forohub.domain.usuario.validation.create;
+
+import com.desafio.forohub.domain.usuario.dto.CrearUsuarioDTO;
+
+public interface ValidarCrearUsuario {
+    void validate(CrearUsuarioDTO data);
+}
